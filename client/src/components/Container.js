4ec53d8fec7aabbd318a683a7612d9b7e8587c07
Samet Koyuncu <sametkoyuncu@live.com>
2021-11-05 19:43:55 +0300

    End of tutorial

--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import { useEffect } from 'react'
 import ChatForm from './ChatForm'
 import ChatList from './ChatList'
+import { useChat } from '../context/ChatContext'
+
+import { init, subscribeChat, subscribeInitialMessages } from '../socketApi'
 
 function Container() {
+  const { setMessages } = useChat()
+  useEffect(() => {
+    init()
+    subscribeInitialMessages((messages) => setMessages(messages))
+    subscribeChat((message) => {
+      setMessages((prevState) => [...prevState, { message }])
+    })
+  }, [])
   return (
     <div className="App">
       <ChatList />
